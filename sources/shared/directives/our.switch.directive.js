eb664b7c00a6e398e8898b10fec1da79c3c584de
cstefanache <cornel.stefanache@gmail.com>
2016-09-28 13:57:36 +0200

    ourswitch example

---
+++ sources/shared/directives/our.switch.directive.js
@@ -0,0 +1,33 @@
+angular.module('shared')
+    .directive('ourSwitch', OurSwitch);
+
+OurSwitch.$inject = ['$compile'];
+
+function OurSwitch($compile) {
+    return {
+        restrict: 'A',
+        scope: true,
+        compile: function (elem, attrs) {
+
+            var cacheContent = elem.html();
+            elem.html('');
+
+            return function ($scope, elem, attrs) {
+                var isolatedScope;
+
+                $scope.$watch(attrs.ourSwitch,
+                    function (newVal, oldVal) {
+                        console.log('newVal', newVal);
+                        if (!newVal) {
+                            isolatedScope = $scope.$new();
+                            elem.append($compile(cacheContent)(isolatedScope));
+                        } else {
+                            elem.html('');
+                            isolatedScope.$destroy();
+                        }
+                    });
+
+            }
+        }
+    }
+}
